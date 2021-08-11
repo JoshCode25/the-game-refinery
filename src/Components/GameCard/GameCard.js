be3bae7ceb348e5fa242e262fb5876c65539ea2a
JoshG <joshcode25@gmail.com>
2021-08-11 13:00:12 -0500

    adjusted game card size and info on hover

--- src/Components/GameCard/GameCard.js
+++ src/Components/GameCard/GameCard.js
@@ -1,31 +1,28 @@
 import React, {useState} from 'react';
-import ExpandButton from '../ExpandButton/ExpandButton';
+import './GameCard.css';
 
 function GameCard({game}) {
-    const [isExpanded, setIsExpanded] = useState(false);
-    const [cardWidth, setCardWidth] = useState('250px');
+    const cardWidth = '210px';
     const {name, rating, playerMin, playerMax, 
         lengthMin, lengthMax, mechanics, description, imageUrl} = game;
 
-    const toggleExpandCard = () => {
-        isExpanded? setCardWidth('250px') : setCardWidth('550px');
-        setIsExpanded(!isExpanded);
-    }
-
     return (
-        <div style={{backgroundColor: 'light-gray', width: cardWidth}} className='flex grow ma3 ba'>
-            <div style={{position: 'relative'}} className='flex flex-column items-center' >
-                <img src={imageUrl} alt='Game Icon' style={{width: '250px'}} />
-                <h2 className='f3 pb1 tc'>{name.toUpperCase()}</h2>
+        <div style={{width: cardWidth, height: cardWidth, position: 'relative'}} className='flex pointer ma3 ba shadowHover'>
+            <div style={{position: 'absolute', top: '10px'}} className='hidden'>
                 <p className='f3 pb1'>Players: {playerMin}{playerMin === playerMax? '': '-' + playerMax}</p>
                 <p className='f3 pb2'>Length: {lengthMin}-{lengthMax}min</p>
-                {!isExpanded? 
-                    <ExpandButton isExpanded={isExpanded} toggleExpanded={toggleExpandCard}/>
-                    : ''
-                }    
-                {/* <p style={{position: 'absolute', left: '15px', top: '15px'}} className='f4'>{rating}/5</p> */}
+                <p style={{position: 'absolute', left: '15px', top: '15px'}} className='f4'>{rating}/5</p>
+
             </div>
-            {isExpanded? 
+            <div className='flex flex-column items-center' >
+                <img src={imageUrl} alt='Game Icon' style={{width: '100%', height: 'auto', overflow: 'hidden'}} />
+                <h2 style={{position: 'absolute', bottom: '10px', backgroundColor: 'white'}} 
+                    className='f3 pb1 tc'
+                    >
+                        {name.toUpperCase()}
+                </h2>
+            </div>
+            {/* {isExpanded? 
                 (
                     <div style={{position: 'relative', width: '300px'}} className='flex flex-column justify-around'>
                         <p className='f3 pa2'>{description}</p>
@@ -38,7 +35,7 @@ function GameCard({game}) {
                         <ExpandButton isExpanded={isExpanded} toggleExpanded={toggleExpandCard}/>
                     </div> 
                 ) : ''
-            }
+            } */}
         </div>
     )
 }
