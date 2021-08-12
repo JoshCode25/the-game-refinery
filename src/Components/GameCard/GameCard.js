38c601599dd6b247fdf6bd77c71227cad35126ff
JoshG <joshcode25@gmail.com>
2021-08-12 13:00:27 -0500

    added game info on hover and started updating game class to accept object for creation

--- src/Components/GameCard/GameCard.js
+++ src/Components/GameCard/GameCard.js
@@ -4,20 +4,23 @@ import './GameCard.css';
 function GameCard({game}) {
     const cardWidth = '210px';
     const {name, rating, playerMin, playerMax, 
-        lengthMin, lengthMax, mechanics, description, imageUrl} = game;
+        lengthMin, lengthMax, mechanics, description, status, imageUrl} = game;
+
+    const ratingStatusDisplay = rating? (`${rating}/5`) : status;
 
     return (
-        <div style={{width: cardWidth, height: cardWidth, position: 'relative'}} className='flex pointer ma3 ba shadowHover'>
-            <div style={{position: 'absolute', top: '10px'}} className='hidden'>
+        <div style={{width: cardWidth, height: cardWidth, position: 'relative'}} className='flex pointer ma3 ba shadowHover hoverReveal'>
+            <div className='hoverInfoContainer'>
+                <p className='f4'>{ratingStatusDisplay}</p>
                 <p className='f3 pb1'>Players: {playerMin}{playerMin === playerMax? '': '-' + playerMax}</p>
-                <p className='f3 pb2'>Length: {lengthMin}-{lengthMax}min</p>
-                <p style={{position: 'absolute', left: '15px', top: '15px'}} className='f4'>{rating}/5</p>
+                <p className='f3 pb1'>Length: {lengthMin}-{lengthMax}min</p>
+                <p className='f3 pb2'>Status: {status}</p>
 
             </div>
             <div className='flex flex-column items-center' >
                 <img src={imageUrl} alt='Game Icon' style={{width: '100%', height: 'auto', overflow: 'hidden'}} />
                 <h2 style={{position: 'absolute', bottom: '10px', backgroundColor: 'white'}} 
-                    className='f3 pb1 tc'
+                    className='f4 pb1 tc mh2'
                     >
                         {name.toUpperCase()}
                 </h2>
--- src/Components/GameList/gameList.js
+++ src/Components/GameList/gameList.js
@@ -14,20 +14,22 @@ const defaultValues = {
     lengthMax:60, 
     mechanics:[], 
     description:'Cool', 
+    status: 'Concept Phase',
     imageUrl:logo
 }
 
 class Game {
-    constructor(name, rating, playerMin, playerMax, lengthMin, lengthMax, mechanics, description, imageUrl) {
-            this.name = name;
-            this.rating = rating;
-            this.playerMin = playerMin;
-            this.playerMax = playerMax;
-            this.lengthMin = lengthMin;
-            this.lengthMax = lengthMax;
-            this.mechanics = mechanics;
-            this.description = description;
-            this.imageUrl = imageUrl;
+    constructor(gameObject) {
+            this.name = gameObject.name;
+            this.rating = gameObject.rating;
+            this.playerMin = gameObject.playerMin;
+            this.playerMax = gameObject.playerMax;
+            this.lengthMin = gameObject.lengthMin;
+            this.lengthMax = gameObject.lengthMax;
+            this.mechanics = gameObject.mechanics;
+            this.description = gameObject.description;
+            this.status = gameObject.status;
+            this.imageUrl = gameObject.imageUrl;
     }
 
     addToGameList() {
@@ -35,34 +37,47 @@ class Game {
     }
 }
 
-const creatNewGame = (name, rating, playerMin, playerMax, lengthMin, lengthMax, mechanics, description, imageUrl) => {
-    let newName = name? name : defaultValues.name;
-    let newRating = rating? rating : defaultValues.rating;
-    let newPlayerMin = playerMin? playerMin : defaultValues.playerMin;
-    let newPlayerMax = playerMax? playerMax : defaultValues.playerMax;
-    let newLengthMin = lengthMin? lengthMin : defaultValues.lengthMin;
-    let newLengthMax = lengthMax? lengthMax : defaultValues.lengthMax;
-    let newMechanics = mechanics? mechanics : defaultValues.mechanics;
-    let newDescription = description? description : defaultValues.description;
-    let newImageUrl = imageUrl? imageUrl : defaultValues.imageUrl;
+const creatNewGame = (gameObject) => {
+    let newGameObject = {};
 
-    let newGame = new Game(newName, newRating, newPlayerMin, newPlayerMax, newLengthMin, newLengthMax, 
-        newMechanics, newDescription, newImageUrl);
+    newGameObject.newName = gameObject.name? gameObject.name : defaultValues.name;
+    newGameObject.newRating = gameObject.rating? gameObject.rating : defaultValues.rating;
+    newGameObject.newPlayerMin = gameObject.playerMin? gameObject.playerMin : defaultValues.playerMin;
+    newGameObject.newPlayerMax = gameObject.playerMax? gameObject.playerMax : defaultValues.playerMax;
+    newGameObject.newLengthMin = gameObject.lengthMin? gameObject.lengthMin : defaultValues.lengthMin;
+    newGameObject.newLengthMax = gameObject.lengthMax? gameObject.lengthMax : defaultValues.lengthMax;
+    newGameObject.newMechanics = gameObject.mechanics? gameObject.mechanics : defaultValues.mechanics;
+    newGameObject.newDescription = gameObject.description? gameObject.description : defaultValues.description;
+    newGameObject.newStatus = gameObject.status? gameObject.status : defaultValues.status;
+    newGameObject.newImageUrl = gameObject.imageUrl? gameObject.imageUrl : defaultValues.imageUrl;
+
+    let newGame = new Game(newGameObject);
     newGame.addToGameList();
     return newGame;
 }
 
-const mythicCircus = creatNewGame('Mythic Circus', 5, 2, 5, 30, 90, 
-    ['Deckbuilding'], 'Build your team to become the greatest Ringleader', mythicIcon);
+const mythicCircus = {
+    name:'Mythic Circus', 
+    rating:4.8, 
+    playerMin:2, 
+    playerMax:5, 
+    lengthMin:30, 
+    lengthMax:90, 
+    mechanics:['Deckbuilding'], 
+    description:'Build your team to become the greatest Ringleader', 
+    status: 'Proto V2.0',
+    imageUrl:mythicIcon
+} 
+creatNewGame(mythicCircus);
 const theFarmGame = creatNewGame('The Farm Game', 4.5, 2, 5, 30, 90, 
     ['Resource Management', 'Set Collection', 'Dice'], 
-    'Plant, Water, Harvest, and Sell your produce to become the most renowned farm',
+    'Plant, Water, Harvest, and Sell your produce to become the most renowned farm', 'Reworking',
     farmIcon);
 const peasantPrinceAndKing = creatNewGame('Peasant Prince & King', 2, 2, 2, 20, 40, 
-    ['Resource Management', 'Duel'], "Develop your kingdom's 4 departments to defeat your opponent", 
+    ['Resource Management', 'Duel'], "Develop your kingdom's 4 departments to defeat your opponent", 'Inactive',
     peasantIcon);
 const theDiceGame = creatNewGame('The Dice Game', 4, 2, 4, 15, 30, 
-    ['Dice', 'Cooperative'], 'Build your local town and work with those around you to overthrow the Tyrant');
+    ['Dice', 'Cooperative'], 'Build your local town and work with those around you to overthrow the Tyrant', 'Proto V1.0');
 
 
 export default gameList;
