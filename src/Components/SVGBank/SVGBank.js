71e34afda4b3dd2f94e3df2ee185b1c078274735
JoshG <joshcode25@gmail.com>
2021-08-24 12:57:16 -0500

    svg arc research

--- src/Components/SVGBank/SVGBank.js
+++ src/Components/SVGBank/SVGBank.js
@@ -167,9 +167,35 @@ export const createClockIcon = (propertyInput) => {
     )
 }
 
+/*
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+  return {
+    x: centerX + (radius * Math.cos(angleInRadians)),
+    y: centerY + (radius * Math.sin(angleInRadians))
+  };
+}
+
+function describeArc(x, y, radius, startAngle, endAngle){
+
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+
+    var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+    var d = [
+        "M", start.x, start.y, 
+        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+    ].join(" ");
+
+    return d;       
+}
+*/
+
 const calcClockCoord = (radius, degree) => {
     
-    let radianX = degree*Math.PI/180;
+    let radianX = (degree-90)*Math.PI/180;
     let radianY = (degree-90)*Math.PI/180
     let x = radius*Math.sin(radianX);
     let y = radius*Math.cos(radianY);
